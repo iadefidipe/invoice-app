17f25131d276ac034d1f26a9a4a30e7d79ab0d86
Israel <israeladefidipe@gmail.com>
2021-12-21 21:40:11 +0100

    feat: global style

---
+++ styles/Globalstyles.ts
@@ -0,0 +1,18 @@
+import styled, { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        background: ${(props) => props.theme.color.body.bg};
+        font-family: 'Spartan', sans-serif;
+        transition: background .3s;
+        -webkit-font-smoothing: antialiased;
+        overflow-x: hidden;
+    }
+`
