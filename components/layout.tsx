cb9a776cc9292cce3b9900c3373b1b720b8a4c67
Israel <israeladefidipe@gmail.com>
2021-12-23 02:42:13 +0100

    feat: redux dependency for app state management - installed redux and redux toolkit for state managment - create store and wrapped global component with the redux provider

--- components/layout.tsx
+++ components/layout.tsx
@@ -44,28 +44,3 @@ function Layout({ children }: layoutType) {
 }
 
 export default Layout
-Hi Offchain Labs Hiring Team,
-
-It is definitely an exciting time to work in the blockchain space. I believe blockchain is the future of the internet and this is clear as the innovations in the space have revolutionized different sectors and it is rapidly gaining massive acceptance.
-
-With over a year of hands-on experience with frontend technologies, building functional and accessible web projects, I can’t think of a more exciting start to my professional growth than exploring and shaping the future with you. What, specifically, do I bring to the team?
-
-Excited to learn and develop blockchain technology:
-
-I- Currently studying and learning about bitcoin and ethereum, and the blockchain. My goal is to build products that scale, are accessible, and are safe.
-
-I- Building competency in solidity and web3.js, and I currently have a simple dapp deployed on the ethereum test net.
-
-Great frontend skills: 
-
-- I  write clean and efficient code with HTML, CSS/SCSS, Javascript (React/Redux, Next.js), typescript. I am also very open to feedback on my work and constantly looking for ways to improve and stay up to date with new technologies.
-
-I am confident that this is a very rare opportunity to grow and draw from the wealth of experience and expertise on your team. I am certain that I would be a valuable asset in your company and I would appreciate the opportunity to prove this to you.
-
-Thank you for your time and consideration. I look forward to speaking with you about this opportunity.
-
-Best Regards,
-
-Israel Adefidipe
-
- 
