7b8881414e8a9e5405dffed1c070e4313013de6d
Israel <israeladefidipe@gmail.com>
2021-12-25 22:43:35 +0100

    fix: sidebar components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,30 @@
-function Sidebar() {
-	return <div></div>
+import styled from 'styled-components'
+import Avatar from './shared/Avatar'
+import Logo from './shared/Logo'
+import { toggleThemeFunc } from '../types/types'
+import ThemeToggle from './ThemeToggle'
+
+const SidebarWrapper = styled.aside`
+	background-color: ${({ theme }) => theme.color.sidebar.bg};
+	position: sticky;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	@media (min-width: 900px) {
+		flex-direction: column;
+	}
+`
+const InnerWrapper = styled.div``
+
+function Sidebar({ toggleTheme }: toggleThemeFunc) {
+	return (
+		<SidebarWrapper>
+			<Logo />
+			<ThemeToggle toggleTheme={toggleTheme} />
+			<Avatar />
+		</SidebarWrapper>
+	)
 }
 
 export default Sidebar
