f902a1d1babb71e56e618ac3c211b59ed57f1f9e
Israel <israeladefidipe@gmail.com>
2022-01-20 02:48:58 +0100

    fix: Helper styles file

---
+++ styles/HelperStyles.tsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components'
+
+export const flex = css`
+	display: flex;
+	align-items: center;
+`
+
+export const Span = styled.span`
+	@media (max-width: 468px) {
+		display: none;
+	}
+`
