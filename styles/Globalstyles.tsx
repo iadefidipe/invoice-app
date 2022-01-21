a9f4945f4c2bf0084d5a73f349ec3490b86ffc51
Israel <israeladefidipe@gmail.com>
2022-01-21 23:16:42 +0100

    fix: global styles file extension corrected

---
+++ styles/Globalstyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        background: ${({ theme }) => theme.color.body.bg};
+        font-family: 'Spartan', sans-serif;
+        transition: background .3s;
+        -webkit-font-smoothing: antialiased;
+        overflow-x: hidden;
+    }
+`
+export default GlobalStyles
