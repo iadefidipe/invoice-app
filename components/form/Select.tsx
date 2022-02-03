17c274be0eb19f23e4f5be6c5d0162cf439970a9
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-03 13:06:17 +0100

    feat: form Select  field

--- components/form/Select.tsx
+++ components/form/Select.tsx
@@ -1,24 +1,72 @@
-import { Field, ErrorMessage } from "formik";
-import React from 'react';
-import styled from "styled-components";
+import { Field, useField } from "formik"
+import React from "react"
+import styled from "styled-components"
+import { fontStylesA } from "../shared/typography"
+import arrow from "../../assets/icon-arrow-down.svg"
 
-const SelectDrop = styled.div``
-const Label = styled.label``
+const SelectDrop = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem;
+  ${fontStylesA}
+`
+const Label = styled.label`
+  color: ${(props) => props.theme.color.text.formLabel};
+  transition: color 0.3s;
+`
+const SelectWrapper = styled.div`
+  position: relative;
+  grid-column: 1 / -1;
+  ::after {
+    content: url(${arrow.src});
+    position: absolute;
+    top: 50%;
+    right: 1rem;
+    transform: translateY(-50%);
+  }
+`
 
-function Select(props) {
-    const { label, name, options, ...rest } = props
-    return(
-        <SelectDrop>
-            <Label htmlFor={name} >{label}</Label>
-            <Field as='select' id={name} name={name} {...rest}>
-                {options.map( option => {
-                    return(
-                        <option key={option.key} value={option.value}>{option.key}</option>
-                    )
-                } )}
-            </Field>
-        </SelectDrop>
-    )
+const StyledSelect = styled.select`
+  width: 100%;
+  border: 1px solid ${(props) => props.theme.color.form.fieldBorder};
+  border-radius: 4px;
+  padding: 1rem 1.25rem;
+  background: ${(props) => props.theme.color.form.fieldBg};
+  appearance: none;
+  outline: none;
+  ${fontStylesA}
+  color: ${(props) => props.theme.color.text.heading};
+  font-weight: bold;
+  cursor: pointer;
+  transition: color 0.3s, border 0.3s, background 0.3s;
+  :focus {
+    border: 1px solid #9277ff;
+  }
+`
+interface SelectInterfce {
+  label: string
+  name: string
+  options: [{ name: string; value: number }]
 }
 
-export default Select;
+function Select({ label, name, options, ...rest }: SelectInterfce) {
+  const [field] = useField(name)
+  return (
+    <SelectDrop>
+      <Label htmlFor={name}>{label}</Label>
+      <SelectWrapper>
+        <StyledSelect {...field}>
+          {options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.name}
+              </option>
+            )
+          })}
+        </StyledSelect>
+      </SelectWrapper>
+    </SelectDrop>
+  )
+}
+
+export default Select
