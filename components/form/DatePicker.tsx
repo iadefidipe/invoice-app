86cac9d867037c4d6694d8a3533d6f42f49cbdae
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-03 13:04:05 +0100

    feat: form Date picker field

--- components/form/DatePicker.tsx
+++ components/form/DatePicker.tsx
@@ -1,34 +1,94 @@
-import React from 'react';
-import DateView from 'react-datepicker'
-import {Field, ErrorMessage} from 'formik'
-import 'react-datepicker/dist/react-datepicker.css'
-import styled from 'styled-components';
+import  { forwardRef } from "react"
+import DatePicker from "react-datepicker"
+import { Field, useFormikContext, useField } from "formik"
+import "react-datepicker/dist/react-datepicker.css"
+import styled from "styled-components"
+import Image from "next/image"
+import calenderIcon from '../../assets/icon-calendar.svg'
+
+import { fontStylesA } from "../shared/typography"
+
+interface DatePickerStyleInterface {
+  valid?: string
+}
+interface FowardrefInterface {
+  value: Date | null
+  onClick: () => void
+}
+interface DatePropInterface{
+  label: string
+  name: string
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  ${fontStylesA}
+  .react-datepicker-wrapper {
+    grid-column: 1 / -1;
+  }
+`
+
+const Label = styled.label<DatePickerStyleInterface>`
+  grid-column: 1 / 2;
+  margin-bottom: 0.625rem;
+  color: ${(props) =>
+    props.valid ? props.theme.color.text.formLabel : "#EC5757"};
+  transition: color 0.3s;
+`
+
+const Button = styled.button<DatePickerStyleInterface>`
+  grid-column: 1 / -1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 3rem;
+  border: 1px solid
+    ${(props) => (props.valid ? props.theme.color.form.fieldBorder : "#EC5757")};
+  border-radius: 4px;
+  padding: 1rem 1.25rem;
+  background: ${(props) => props.theme.color.form.fieldBg};
+  ${fontStylesA}
+  color: ${(props) => props.theme.color.text.heading};
+  font-weight: bold;
+  outline: none;
+  cursor: pointer;
+  transition: color 0.3s, border 0.3s, background 0.3s;
+  :focus {
+    border: 1px solid #9277ff;
+  }
+`
 
 
+function DateInput({ label, name }: DatePropInterface ){
+  const { setFieldValue } = useFormikContext()
+  const [field, meta] = useField(name)
 
-function DatePicker(props) {
-  const { label, name, ...rest } = props
+  const CustomInput = forwardRef<HTMLButtonElement, FowardrefInterface > (({ value, onClick }, ref) => (
+    <Button type='button' onClick={onClick} ref={ref} valid='true'>
+      <span>{value}</span>
+      <Image src={calenderIcon}  alt='' />
+    </Button>
+  ))
+
   return (
-    <div className='form-control'>
-    <label htmlFor={name}>{label}</label>
-    <Field name={name}>
-      {({ form, field }) => {
-        const { setFieldValue } = form
-        const { value } = field
-        return (
-          <DateView
-            id={name}
-            {...field}
-            {...rest}
-            selected={value}
-            onChange={val => setFieldValue(name, val)}
-          />
-        )
-      }}
-    </Field>
-    <ErrorMessage  name={name} />
-  </div>
+    <Wrapper>
+      <Label htmlFor={name} valid='true'>
+        {label}
+      </Label>
+      <DatePicker
+        id={name}
+        {...field}
+        selected={field.value}
+        onChange={(value) => setFieldValue(name, value)}
+        customInput={<CustomInput />}
+        dateFormat='MMM d, yyyy'
+        showYearDropdown
+        scrollableMonthYearDropdown
+      />
+    </Wrapper>
   )
 }
 
-export default DatePicker;
+export default DateInput
