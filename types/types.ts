51bb1a86a64d431210b6fd70d23ecf601a3992a8
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-03 13:52:32 +0100

    feat: updated types

--- types/types.ts
+++ types/types.ts
@@ -68,3 +68,35 @@ export interface StyleInterface {
 export interface toggleThemeFunc {
 	toggleTheme(): void
 }
+
+export interface InvoiceInterface{
+	"id": string,
+	"createdAt": string,
+	"paymentDue": string,
+	"description": string,
+	"paymentTerms": number,
+	"clientName": string,
+	"clientEmail": string,
+	"status": string,
+	"senderAddress": {
+		"street": string,
+		"city": string,
+		"postCode": string,
+		"country": string
+	},
+	"clientAddress": {
+		"street": string,
+		"city": string,
+		"postCode": string,
+		"country": string
+	},
+	"items": [
+		{
+			"name": string,
+			"quantity": number,
+			"price": number,
+			"total": number
+		}
+	],
+	"total": number
+}
