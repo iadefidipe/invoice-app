d9256c72289aaaa855166bc72616da5e85b3db3b
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-03 13:07:51 +0100

    feat: redux store updated with new actions

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './features/theme'
 import filterReducer from './features/filter'
+import invoiceReducer from './features/invoices'
+import openReducer from './features/open'
 
 // creating store with redux toolkit
 export const store = configureStore({
 	reducer: {
 		theme: themeReducer,
 		filter: filterReducer,
+		invoice: invoiceReducer,
+		open:openReducer
 	},
 })
 
