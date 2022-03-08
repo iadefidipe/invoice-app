2a9c7ffb8bb19b291440a734b1b1c9012d0e2e2b
Israel <israeladefidipe@gmail.com>
2022-03-08 12:57:25 +0100

    fix: backdrop when from is open

--- components/form/Backdrop.tsx
+++ components/form/Backdrop.tsx
@@ -1,40 +1,49 @@
-// import styled from 'styled-components'
-// import ScrollLock from 'react-scrolllock'
-// import { motion } from 'framer-motion'
+import styled from "styled-components"
+import ScrollLock from "react-scrolllock"
+import { motion } from "framer-motion"
+import { useAppDispatch } from "redux/types/reduxTypes"
+import { toggleForm } from "redux/features/openForm"
 
-// const StyledBackdrop = styled(motion.div)`
-//     position: fixed;
-//     top: 0;
-//     left: 0;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     width: 100%;
-//     height: 100vh;
-//     z-index: 5;
-//     background: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5));
-// `
+const StyledBackdrop = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  z-index: 5;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+  border: 2px green solid;
+`
 
-// const animation = {
-//     hidden: { opacity: 0 },
-//     visible: { 
-//         opacity: 1,
-//         transition: { duration: .3 }
-//     }
-// }
+const animation = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.3 },
+  },
+}
 
-// export default function Backdrop({ setIsOpen, children }) {
-//     return (
-//         <ScrollLock>
-//             <StyledBackdrop
-//                 variants={animation}
-//                 initial="hidden"
-//                 animate="visible"
-//                 exit="hidden"
-//                 onClick={() => setIsOpen(false)}
-//             >
-//                 {children}
-//             </StyledBackdrop>
-//         </ScrollLock>
-//     )
-// }
+interface BackdropInterface {
+    children: React.ReactNode
+}
+
+export default function Backdrop({ children }:BackdropInterface) {
+  const dispatch = useAppDispatch()
+
+  return (
+    <ScrollLock>
+      <StyledBackdrop
+        variants={animation}
+        initial='hidden'
+        animate='visible'
+        exit='hidden'
+        onClick={() => dispatch(toggleForm(false))}
+      >
+        {children}
+      </StyledBackdrop>
+    </ScrollLock>
+  )
+}
