d088db0841c91009a9c18e8ace55f5954ac830cb
Israel <israeladefidipe@gmail.com>
2022-03-09 10:02:28 +0100

    feat: new changes to invoice details component - fixed styling issues - added create edit form and delete popus component

--- components/invoiceDetails/InvoiceDetail.tsx
+++ components/invoiceDetails/InvoiceDetail.tsx
@@ -6,30 +6,62 @@ import Image from "next/image"
 import arrow from "../../assets/icon-arrow-left.svg"
 import { fontStylesA, fontStylesB } from "components/shared/typography"
 import InvoiceStatus from "components/shared/InvoiceStatus"
-import Button from "components/shared/Buttons"
 import Link from "next/link"
 import { invoicesMessage } from "utilities/Misc"
-import { Id } from "components/home/invoice/invoiceItem.style"
-import { Heading1,Heading2, Heading3, Heading4 } from 'components/shared/Headings'
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+} from "components/shared/Headings"
+import InvoiceTable from "./InvoiceTable"
+import Buttons from "./Buttons"
+import CreateEditForm from "components/form/CreateEditForm"
+import { FormDataInterface } from "data/form"
+import DeletePopup from "./DeletePopup"
+
 
 const Wrapper = styled(InnerWrapper)`
-  border: 2px red solid;
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
 `
-const BackLink = styled(Link)`
-  cursor: pointer;
-`
-const InvoiceHeader = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  background: ${(props) => props.theme.color.invoiceItem.bg};
-  width: 100%;
+const GenStyle = css`
   padding: 1rem 1.5rem;
 `
+const BackLink = styled(Link)``
+const InvoiceHeader = styled.div`
+  display: grid;
+  background: ${(props) => props.theme.color.invoiceItem.bg};
+  width: 100%;
+  border-radius: 0.5rem;
+  ${GenStyle}
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  & > div:nth-of-type(2) {
+    display: none;
+    @media only screen and (min-width: 768px) {
+      grid-columnn: 2/3;
+      display: flex;
+    }
+  }
+`
 const InvoiceFooter = styled.div`
-  display: flex;
+  padding: 2rem;
+  background: ${(props) => props.theme.color.invoiceItem.bg};
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`
+const FooterWrap = styled.div`
+  margin: 0 auto;
 `
 
-const LinkWrap = styled.div``
+const LinkWrap = styled.div`
+  cursor: pointer;
+`
 const LinkText = styled.span`
   ${fontStylesA}
 `
@@ -37,14 +69,14 @@ const LinkText = styled.span`
 const StatusWrap = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 1rem;
-`
-const ButtonWrap = styled(StatusWrap)`
-  display: none;
   @media only screen and (min-width: 768px) {
-    display: block;
+    grid-columnn: 1/2;
+    justify-content: start;
   }
 `
+
 const StatusText = styled.p`
   ${fontStylesB}
 `
@@ -52,102 +84,145 @@ const DetailBold = styled(Heading3)``
 
 const StatusInvoice = styled(InvoiceStatus)``
 
-const InvoiceBody = styled.div``
+const InvoiceBody = styled.div`
+  ${GenStyle}
+  display: grid;
+  gap: 31px;
+`
 const Description = styled.div``
 const DescriptionText = styled.p`
   ${fontStylesB}
 `
 const Address = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
   & p {
     ${fontStylesA}
   }
 `
+const Id = styled(Heading2)`
+  font-size: 0.75rem;
+  span {
+    color: #7e88c3;
+  }
+`
 
-const Details = styled.div``
+const Details = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(3, 1fr);
+
+  & div:nth-of-type(4) {
+    grid-column: 1/3;
+    margin-top: 2rem;
+  }
+
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1.5fr;
+    grid-template-rows: repeat(2, 1fr);
+    & div:nth-of-type(2) {
+      grid-column: 1/2;
+    }
+
+    & div:nth-of-type(4) {
+      grid-column: 3/4;
+      grid-row: 1/2;
+      margin-top: 0;
+    }
+  }
+`
 
 const StyledDetail = styled.div``
 const PaymentDue = styled.div``
-const BillTo = styled.div``
-const DetailHeading = styled.p`
-${fontStylesA}
+const BillTo = styled.div`
+  grid-column: 2/ 3;
+  grid-row: 1/3;
 `
+const DetailHeading = styled.p`
+  ${fontStylesA}
+  margin-bottom: 12px;
+`
+const InvoiceDescriptionWrap = styled.div``
 interface InvoiceDetailInterface {
-  data: InvoiceInterface
+  data: FormDataInterface
 }
 
 function InvoiceDetail({ data }: InvoiceDetailInterface) {
   console.log("status", data.clientName)
   return (
-    <Wrapper>
-      <BackLink href={`/`} passHref={true}>
-        <LinkWrap>
-          <Image src={arrow} alt='' /> <LinkText>Go Back</LinkText>
-        </LinkWrap>
-      </BackLink>
+    <>
+    <DeletePopup id = {data.id}/>
+      <CreateEditForm invoice={data} />
+      <Wrapper>
+        <BackLink href={`/`} passHref={true}>
+          <LinkWrap>
+            <Image src={arrow} alt='' /> <LinkText>Go Back</LinkText>
+          </LinkWrap>
+        </BackLink>
 
-      <InvoiceHeader>
-        <StatusWrap>
-          <StatusText>Status</StatusText>
-          <StatusInvoice status={data.status} />
-        </StatusWrap>
+        <InvoiceHeader>
+          <StatusWrap>
+            <StatusText>Status</StatusText>
+            <StatusInvoice status={data.status} />
+          </StatusWrap>
 
-        <ButtonWrap>
-          <Button secondary>Edit</Button>
-          <Button alert>Delete</Button>
-          {!(data.status === "paid") && <Button>Mark as Paid</Button>}
-        </ButtonWrap>
-      </InvoiceHeader>
+          <Buttons />
+        </InvoiceHeader>
 
-      <InvoiceBody>
-        <Description>
-          <Id>
-            <span>#</span>
-            {data.id.slice(0, 5)}
-          </Id>
-          <DescriptionText>{data.description}</DescriptionText>
-        </Description>
+        <InvoiceBody>
+          <InvoiceDescriptionWrap>
+            <Description>
+              <Id>
+                <span>#</span>
+                {data.id.slice(0, 5)}
+              </Id>
+              <DescriptionText>{data.description}</DescriptionText>
+            </Description>
 
-        <Address>
-          <p>{data.senderAddress.street}</p>
-          <p>{data.senderAddress.city}</p>
-          <p>{data.senderAddress.postCode}</p>
-          <p>{data.senderAddress.country}</p>
-        </Address>
-
-        <Details>
-          <StyledDetail>
-            <DetailHeading>Invoice Date</DetailHeading>
-            <DetailBold>{data.createdAt}</DetailBold>
-          </StyledDetail>
-          <StyledDetail>
-            <DetailHeading>PaymentDue</DetailHeading>
-            <DetailBold>{data.paymentDue}</DetailBold>
-          </StyledDetail>
-          <BillTo>
-            <DetailHeading>Bill To</DetailHeading>
-            <DetailBold>{data.clientName}</DetailBold>
             <Address>
-              <p>{data.clientAddress.street}</p>
-              <p>{data.clientAddress.city}</p>
-              <p>{data.clientAddress.postCode}</p>
-              <p>{data.clientAddress.country}</p>
+              <p>{data.senderAddress.street}</p>
+              <p>{data.senderAddress.city}</p>
+              <p>{data.senderAddress.postCode}</p>
+              <p>{data.senderAddress.country}</p>
             </Address>
-          </BillTo>
+          </InvoiceDescriptionWrap>
 
-          <StyledDetail>
-            <DetailHeading>Send To</DetailHeading>
-            <DetailBold>{data.clientEmail}</DetailBold>
-          </StyledDetail>
-        </Details>
-      </InvoiceBody>
-      {/* <InvoiceFooter>
-        <ButtonWrap>
-          <Button secondary>Edit</Button>
-          <Button alert>Delete</Button>
-          <Button>Mark as Paid</Button>
-        </ButtonWrap>
-      </InvoiceFooter> */}
-    </Wrapper>
+          <Details>
+            <StyledDetail>
+              <DetailHeading>Invoice Date</DetailHeading>
+              <DetailBold>{data.createdAt}</DetailBold>
+            </StyledDetail>
+            <StyledDetail>
+              <DetailHeading>PaymentDue</DetailHeading>
+              <DetailBold>{data.paymentDue}</DetailBold>
+            </StyledDetail>
+            <BillTo>
+              <DetailHeading>Bill To</DetailHeading>
+              <DetailBold>{data.clientName}</DetailBold>
+              <Address>
+                <p>{data.clientAddress.street}</p>
+                <p>{data.clientAddress.city}</p>
+                <p>{data.clientAddress.postCode}</p>
+                <p>{data.clientAddress.country}</p>
+              </Address>
+            </BillTo>
+
+            <StyledDetail>
+              <DetailHeading>Send To</DetailHeading>
+              <DetailBold>{data.clientEmail}</DetailBold>
+            </StyledDetail>
+          </Details>
+
+          <InvoiceTable items={data.items} total={data.total} />
+        </InvoiceBody>
+      </Wrapper>
+      <InvoiceFooter>
+        <FooterWrap>
+          <Buttons />
+        </FooterWrap>
+      </InvoiceFooter>
+    </>
   )
 }
 
