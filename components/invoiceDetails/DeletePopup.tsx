5cf85d5f724cfc3a9baa669893848406826b12c7
Israel <israeladefidipe@gmail.com>
2022-04-14 14:47:53 +0100

    feat: Invoice Details component -refactored - reviewed and bugs corrected

--- components/invoiceDetails/DeletePopup.tsx
+++ components/invoiceDetails/DeletePopup.tsx
@@ -1,138 +1,146 @@
-import styled from 'styled-components'
-import ScrollLock from 'react-scrolllock'
-import { motion, AnimatePresence } from 'framer-motion'
-
-import Button from '../shared/Buttons'
-import { Heading2 } from '../shared/Headings'
-import { fontStylesA } from '../shared/typography'
+import styled from "styled-components"
+import ScrollLock from "react-scrolllock"
+import { motion, AnimatePresence } from "framer-motion"
+import Button from "../shared/Buttons"
+import { Heading2 } from "../shared/Headings"
+import { fontStylesA } from "../shared/typography"
 import { useAppDispatch, useAppSelector } from "redux/types/reduxTypes"
-import {popUp} from 'redux/features/popUp'
-import { addDoc, getDocs } from "firebase/firestore"
-import { invoiceCollectionRef } from "pages"
+import { popUp } from "redux/features/popUp"
+import { updateInvoice } from "redux/features/Invoices"
+import { getDocs, doc, deleteDoc } from "firebase/firestore"
+import { db } from "../../firebase/clientApp"
+import Store from "store"
 
+import { invoiceCollectionRef } from "pages"
+import { useRouter } from "next/router"
 
 const Backdrop = styled(motion.div)`
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100vh;
-    z-index: 100;
-    background: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5));
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  z-index: 100;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
 `
 
 const Popup = styled(motion.div)`
-    width: 100%;
-    max-width: 24rem;
-    margin: 0 1.5rem;
-    border-radius: 8px;
-    padding: 2rem;
-    background: ${props => props.theme.color.popup.bg};
-    transition: background .3s;
-    @media only screen and (min-width: 768px) {
-        max-width: 30rem;
-        padding: 3rem;
-    }
+  width: 100%;
+  max-width: 24rem;
+  margin: 0 1.5rem;
+  border-radius: 8px;
+  padding: 2rem;
+  background: ${(props) => props.theme.color.popup.bg};
+  transition: background 0.3s;
+  @media only screen and (min-width: 768px) {
+    max-width: 30rem;
+    padding: 3rem;
+  }
 `
 
 const Heading = styled(Heading2)`
-    margin-bottom: 1rem;
-    @media only screen and (min-width: 768px) {
-        margin-bottom: 1.5rem;
-        font-size: 1.5rem;
-    }
+  margin-bottom: 1rem;
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+  }
 `
 
 const Message = styled.p`
-    margin-bottom: 1.5rem;
-    ${fontStylesA}
-    line-height: 1.375rem;
-    @media only screen and (min-width: 768px) {
-        margin-bottom: 1rem;
-    }
+  margin-bottom: 1.5rem;
+  ${fontStylesA}
+  line-height: 1.375rem;
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 1rem;
+  }
 `
 
 const Buttons = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    gap: .5rem;
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5rem;
 `
 
 const backdropAnimation = {
-    hidden: { opacity: 0 },
-    visible: { 
-        opacity: 1,
-        transition: { duration: .15 }
-    },
-    exit: { 
-        opacity: 0,
-        transition: { duration: .15 }
-    }
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.15 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.15 },
+  },
 }
 
 const popupAnimation = {
-    hidden: { scale: 0 },
-    visible: { 
-        scale: 1,
-        transition: { type: 'spring', duration: .35 }
-    },
-    exit: { 
-        scale: 0,
-        transition: { duration: .15 }
-    }
+  hidden: { scale: 0 },
+  visible: {
+    scale: 1,
+    transition: { type: "spring", duration: 0.35 },
+  },
+  exit: {
+    scale: 0,
+    transition: { duration: 0.15 },
+  },
 }
 
-export default function DeletePopup({id}) {
-    const dispatch = useAppDispatch()
-    const popupOpen = useAppSelector ( (state) => state.popUpOpen.value ) 
-    const invoice = useAppSelector( (state) => state.invoice.value )
+interface DeletePopupInterface {
+  id: string
+}
 
+export default function DeletePopup({ id }: DeletePopupInterface) {
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const popupOpen = useAppSelector((state) => state.popUpOpen.value)
 
-    const deleteInvoice = async (id) => {
-        
-        const data = await getDocs(invoiceCollectionRef)
-        
-        const newInvoices = data.docs.filter( (doc) => {
-            
-        } )
-
-
-
-
-
-        Store.set(
-            "invoices",
-            data.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-          )
-          dispatch(updateInvoice(Store.get("invoices")))
-        
-        dispatch(popUp(false))
-      }
-
-    return (
-        <AnimatePresence>
-            {popupOpen && 
-                <ScrollLock>
-                    <Backdrop
-                        variants={backdropAnimation}
-                        initial="hidden"
-                        animate="visible"
-                        exit="exit"
-                    >
-                        <Popup variants={popupAnimation}>
-                            <Heading>Confirm Deletion</Heading>
-                            <Message>Are you sure you want to delete invoice {id}? This action cannot be undone.</Message>
-                            <Buttons>
-                                <Button secondary onClick={() => dispatch(popUp(false))}>Cancel</Button>
-                                <Button alert onClick={ () => deleteInvoice(id, invoice) }>Delete</Button>
-                            </Buttons>
-                        </Popup>
-                    </Backdrop>
-                </ScrollLock>
-            }
-        </AnimatePresence>
+  //Delete Invovoice based on ID
+  const deleteInvoice = async (id: string) => {
+    const invoiceDoc = doc(db, "invoices", id)
+    await deleteDoc(invoiceDoc)
+    const data = await getDocs(invoiceCollectionRef)
+    Store.set(
+      "invoices",
+      data.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
     )
+    dispatch(updateInvoice(Store.get("invoices")))
+    dispatch(popUp(false))
+
+    //go back to homempage
+    router.push("/")
+  }
+
+  return (
+    <AnimatePresence>
+      {popupOpen && (
+        <ScrollLock>
+          <Backdrop
+            variants={backdropAnimation}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+          >
+            <Popup variants={popupAnimation}>
+              <Heading>Confirm Deletion</Heading>
+              <Message>
+                Are you sure you want to delete invoice {id}? This action cannot
+                be undone.
+              </Message>
+              <Buttons>
+                <Button secondary onClick={() => dispatch(popUp(false))}>
+                  Cancel
+                </Button>
+                <Button alert onClick={() => deleteInvoice(id)}>
+                  Delete
+                </Button>
+              </Buttons>
+            </Popup>
+          </Backdrop>
+        </ScrollLock>
+      )}
+    </AnimatePresence>
+  )
 }
