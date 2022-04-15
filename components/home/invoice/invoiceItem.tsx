bd9c706d070a12c92ac4b8a11fb42950c389c1e1
Israel <israeladefidipe@gmail.com>
2022-04-16 00:05:32 +0100

    fix: solved type checking errors

--- components/home/invoice/invoiceItem.tsx
+++ components/home/invoice/invoiceItem.tsx
@@ -1,46 +1,50 @@
-import Link from 'next/link'
+import Link from "next/link"
 import {
-	StyledLink,
-	Id,
-	PaymentDue,
-	ClientName,
-	Total,
-	StyledInvoiceStatus,
-	Arrow,
-	animation,
-} from './invoiceItem.style'
-import dayjs from 'dayjs'
-import arrow from 'assets/icon-arrow-right.svg'
-import Image from 'next/image'
+  StyledLink,
+  Id,
+  PaymentDue,
+  ClientName,
+  Total,
+  StyledInvoiceStatus,
+  Arrow,
+  animation,
+} from "./invoiceItem.style"
+import dayjs from "dayjs"
+import arrow from "assets/icon-arrow-right.svg"
+import Image from "next/image"
 
-
-interface InvoiceItemInterface{
-	id?: string
-	paymentDue: Date
-	clientName: string
-	total: number
-	status: string
-
+interface InvoiceItemInterface {
+  id: string
+  paymentDue: Date
+  clientName: string
+  total: number
+  status: string
 }
 
-function InvoiceItem({id,paymentDue,clientName,total,status}:InvoiceItemInterface): JSX.Element {
-	return (
-		<Link href={`/invoice/${id}`} passHref={true} >
-			<StyledLink variants={animation}>
-				<Id>
-					<span>#</span>
-					{id.slice(0,5)}
-				</Id>
-				<PaymentDue>Due {dayjs(paymentDue).format('DD MMM YYYY')}</PaymentDue>
-				<ClientName>{clientName}</ClientName>
-				<Total as='div'>{total}</Total>
-				<StyledInvoiceStatus status={status} />
-				<Arrow>
-					<Image src={arrow} alt='' />
-				</Arrow>
-			</StyledLink>
-		</Link>
-	)
+function InvoiceItem({
+  id,
+  paymentDue,
+  clientName,
+  total,
+  status,
+}: InvoiceItemInterface): JSX.Element {
+  return (
+    <Link href={`/invoice/${id}`} passHref={true}>
+      <StyledLink variants={animation}>
+        <Id>
+          <span>#</span>
+          {id.slice(0, 5)}
+        </Id>
+        <PaymentDue>Due {dayjs(paymentDue).format("DD MMM YYYY")}</PaymentDue>
+        <ClientName>{clientName}</ClientName>
+        <Total as='div'>{total}</Total>
+        <StyledInvoiceStatus status={status} />
+        <Arrow>
+          <Image src={arrow} alt='' />
+        </Arrow>
+      </StyledLink>
+    </Link>
+  )
 }
 
 export default InvoiceItem
