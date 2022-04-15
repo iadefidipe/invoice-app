8fd7cf0e1eed286c5e47121ab05dc854baa483a7
Israel <israeladefidipe@gmail.com>
2022-04-15 12:45:36 +0100

    fix: pages component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout/layout'
-import { Provider } from 'react-redux'
-import { store } from '../redux/store'
-
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Provider store={store}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
-	)
+import type { AppProps } from "next/app"
+import Layout from "../components/layout/layout"
+import { Provider } from "react-redux"
+import { store } from "../redux/store"
+import { AnimatePresence } from 'framer-motion'
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </Provider>
+  )
 }
 
 export default MyApp
