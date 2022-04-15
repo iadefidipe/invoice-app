6fe17989c7d17bae5ae751b49ca7f329b93d9fb0
Israel <israeladefidipe@gmail.com>
2022-04-15 12:39:58 +0100

    fix: app utility functions

--- utilities/form.tsx
+++ utilities/form.tsx
@@ -1,21 +1,21 @@
 import dayjs from 'dayjs'
 
-export function reduceErrors(errors) {
-    const messages = []
-    for (const key in errors) {
-        const value = errors[key]
-        if (typeof value === 'string') {
-            messages.push(value)
-        } else if (typeof value === 'object') {
-            messages.push(...reduceErrors(value))
-        } else if (Array.isArray(value)) {
-            for (const item of value) {
-                messages.push(...reduceErrors(item))
-            }
-        }
-    }
-    return [...new Set(messages)]
-}
+// export function reduceErrors(errors) {
+//     const messages = []
+//     for (const key in errors) {
+//         const value = errors[key]
+//         if (typeof value === 'string') {
+//             messages.push(value)
+//         } else if (typeof value === 'object') {
+//             messages.push(...reduceErrors(value))
+//         } else if (Array.isArray(value)) {
+//             for (const item of value) {
+//                 messages.push(...reduceErrors(item))
+//             }
+//         }
+//     }
+//     return [...new Set(messages)]
+// }
 
 export function calcTotal(items:any) {
     let total = 0
@@ -25,7 +25,7 @@ export function calcTotal(items:any) {
     return total
 }
 
-export function createInvoice( values) {
+export function createInvoice( values: any) {
     return ({
         ...values,
         createdAt: dayjs(values.createdAt).format('YYYY-MM-DD'),
