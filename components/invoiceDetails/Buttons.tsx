efc473fac1d3d2c8c572c2b8de6c24f89a2f6366
Israel <israeladefidipe@gmail.com>
2022-04-15 12:25:40 +0100

    fix: reviewed invoice button comnponent

--- components/invoiceDetails/Buttons.tsx
+++ components/invoiceDetails/Buttons.tsx
@@ -3,14 +3,33 @@ import Button from "components/shared/Buttons"
 import { useAppDispatch, useAppSelector } from "redux/types/reduxTypes"
 import { toggleEditForm } from "redux/features/openEditForm"
 import { popUp } from "redux/features/popUp"
+import { invoiceCollectionRef } from "pages"
+import {  getDocs, doc, updateDoc } from "firebase/firestore"
+import { db } from "../../firebase/clientApp"
+import { updateInvoice } from "redux/features/Invoices"
+import { InvoiceInterface } from "data/form"
+import { getInvoice } from "utilities/Misc"
 
 const ButtonWrap = styled.div`
   display: flex;
+  justify-content: end;
   gap: 0.5rem;
-  justify-content: center;
+  border: 2px green solid;
 `
-function Buttons() {
+
+interface ButtonsPropInterface {
+  data: InvoiceInterface
+}
+function Buttons({ data }: ButtonsPropInterface) {
   const dispatch = useAppDispatch()
+
+  const handlePaid = async (data: InvoiceInterface) => {
+    const invoiceDoc = await doc(db, "invoices", data.id)
+    await updateDoc(invoiceDoc, { ...data, status: "paid" })
+    const invoiceData = await getDocs(invoiceCollectionRef)
+
+    dispatch(updateInvoice(getInvoice(invoiceData)))
+  }
 
   return (
     <ButtonWrap>
@@ -20,7 +39,9 @@ function Buttons() {
       <Button alert onClick={() => dispatch(popUp(true))}>
         Delete
       </Button>
-      <Button>Mark as Paid</Button>
+      {!(data?.status === "paid") && (
+        <Button onClick={() => handlePaid(data)}>Mark as Paid</Button>
+      )}
     </ButtonWrap>
   )
 }
